5883a15779a12534a82ece5db2bca42aa24cccbf
martin <mart12r2@stud.kea.dk>
2019-09-11 17:30:58 +0200

    PUSH button is now displaying the MODAL and I can click it off via .js

--- mystyletile.js
+++ mystyletile.js
@@ -1,27 +1,14 @@
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+	modal.classList.remove("hide")
+})
 const modal = document.querySelector(".modal-background");
+
 modal.addEventListener("click", () => {
 	modal.classList.add("hide");
 
+	button.addEventListener("click", () => {
+		modal.classList.remove("hide")
+	})
+
 });
-
-copy.querySelector("button").addEventListener("click", () => {
-	fetch(`https://spreadsheets.google.com/feeds/list/${sheetID}`)
-		.then(res => res.json())
-		.then(showVenue);
-	console.log("working...")
-})
-
-document.querySelector("#" + data.category).appendChild(copy);
-
-
-
-//onst sheetID = "100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
-//
-//const link = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
-//
-//fetch(link).then(res => res.json()).then(showVenue);
-//
-//function showVenue(data) {
-//	console.log(data.feed.entry);
-//
-//}
